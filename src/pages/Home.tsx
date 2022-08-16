d0e7d3f08d844d6978d564aad8a440c0160dc96b
1Computer <onecomputer00@gmail.com>
2022-08-15 20:11:59 -0400

    Rework home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,25 +23,11 @@ export default function App() {
 			</div>
 			<div className="flex flex-col justify-center items-center min-h-screen gap-y-4 lg:gap-y-8 py-4 px-2">
 				<h1 className="text-4xl lg:text-6xl text-center">Riichi Tracker</h1>
+				<h2 className="text-xl lg:text-2xl text-center">Keep track of your games!</h2>
 				<div className="flex flex-row justify-center items-start gap-x-8">
 					<div className="flex flex-col justify-center items-center gap-y-2 lg:gap-y-4">
-						<span className="flex flex-col justify-center items-center">
-							<h2 className="text-2xl lg:text-4xl text-center">Games</h2>
-							<p className="text-center">Track your Riichi Mahjong games!</p>
-						</span>
 						<div className="flex flex-col justify-center items-center gap-y-2 lg:gap-y-4">
-							<Button disabled>New Game</Button>
-							<Button disabled>Load Game</Button>
-							<Button disabled>History</Button>
-						</div>
-					</div>
-					<div className="flex flex-col justify-center items-center gap-y-2 lg:gap-y-4">
-						<span className="flex flex-col justify-center items-center">
-							<h2 className="text-2xl lg:text-4xl text-center">Tools</h2>
-							<p className="text-center">Score can be kept separate from full games.</p>
-						</span>
-						<div className="flex flex-col justify-center items-center gap-y-2 lg:gap-y-4">
-							<Button onClick={() => navigate('/calculator', { replace: true })}>Calculator</Button>
+							<Button onClick={() => setOpenNewCompassDialog(true)}>New Game</Button>
 							<Button
 								disabled={toolsGame == null || !toolsGame.ok}
 								onClick={() => {
@@ -49,14 +35,46 @@ export default function App() {
 									navigate('/compass', { state, replace: true });
 								}}
 							>
-								Continue Compass
+								Continue
 							</Button>
-							<Button onClick={() => setOpenNewCompassDialog(true)}>New Compass</Button>
-							{openNewCompassDialog && <NewCompassDialog onClose={() => setOpenNewCompassDialog(false)} />}
+							<Button onClick={() => navigate('/calculator', { replace: true })}>Calculator</Button>
 						</div>
 					</div>
 				</div>
+				<ul className="text-base lg:text-xl flex flex-col justify-center items-start gap-y-1 lg:gap-y-2 list-disc px-6">
+					<li>
+						Create a <span className="text-amber-700 dark:text-amber-500">Compass</span> by using the{' '}
+						<span className="text-amber-700 dark:text-amber-500">New Game</span> button.
+					</li>
+					<li>
+						Add riichi sticks by tapping on the <span className="text-amber-700 dark:text-amber-500">Riichi</span>{' '}
+						button.
+					</li>
+					<li>
+						Transfer scores by tapping on the{' '}
+						<span className="text-amber-700 dark:text-amber-500">winning player&apos;s wind tile</span> and using the{' '}
+						<span className="text-amber-700 dark:text-amber-500">Calculator</span>.
+					</li>
+					<li>
+						Handle draws and repeats by tapping on the{' '}
+						<span className="text-amber-700 dark:text-amber-500">center wind tile</span>.
+					</li>
+					<li>
+						Manually edit scores by tapping on a{' '}
+						<span className="text-amber-700 dark:text-amber-500">player&apos;s score display</span>.
+					</li>
+					<li>
+						Manually edit seats, rounds, and sticks by tapping on the{' '}
+						<span className="text-amber-700 dark:text-amber-500">gear button</span>.
+					</li>
+					<li>Place your phone at the center of the table and enjoy!</li>
+					<li>
+						You can also use the <span className="text-amber-700 dark:text-amber-500">Calculator</span> by itself
+						outside of a game.
+					</li>
+				</ul>
 			</div>
+			{openNewCompassDialog && <NewCompassDialog onClose={() => setOpenNewCompassDialog(false)} />}
 		</div>
 	);
 }
