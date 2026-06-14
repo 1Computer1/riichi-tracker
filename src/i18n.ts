55033ec131f9f83904f3f362fa4d44a3413971f7
1Computer <onecomputer00@gmail.com>
2026-06-14 19:09:35 -0400

    Set fallbackLng to en

--- src/i18n.ts
+++ src/i18n.ts
@@ -8,7 +8,7 @@ void i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: false,
+    fallbackLng: "en",
     returnEmptyString: false,
     interpolation: {
       escapeValue: false,
